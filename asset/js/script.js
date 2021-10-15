6129e630daf276e81d0364a3b04df829ad1d6f4e
Gaetan Peronnet <gaetan.peronnet@gmail.com>
2021-10-15 09:34:00 +0200

    media du sytème de validation

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,11 +1,11 @@
-var mesBoutons = document.querySelectorAll('#adduiux, #addphp, #addreactjs, #b#addnodejs, ##addmysql')
+var mesBoutons = document.querySelectorAll('#adduiux, #addphp, #addreactjs, #addnodejs, #addmysql')
 var textnotif = document.querySelector('#notification')
 for(let i = 0; i < mesBoutons.length; i++){
 
     mesBoutons[i].addEventListener("click",function(){
         textnotif.innerHTML = 'à été ajouté au panier'
         document.getElementById('notification').style.visibility = 'visible'
-        setInterval(function(){
+        setTimeout(function(){
             document.getElementById('notification').style.visibility = 'hidden'
         }, 3000)
     })
